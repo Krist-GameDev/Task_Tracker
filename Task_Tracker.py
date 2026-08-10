5b5da9b652a39e5b4065aa3b35607dba1e651826
Krist-GameDev <artcengler8@gmail.com>
2026-08-10 14:13:20 +0300

    Add files via upload

---
+++ Task_Tracker.py
@@ -0,0 +1,11 @@
+import sys
+def Task_Tracker():
+    if len(sys.argv):
+        print('')
+
+task = {
+    "id": task_id,
+    "description": task_description,
+    "status": task_status,
+    "createdAt": task_
+}
